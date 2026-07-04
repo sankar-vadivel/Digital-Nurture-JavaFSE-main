d28c21403ad09929caaa3149e602ade668a8f123
sankar-vadivel <sankarvadivel07@gmail.com>
2026-07-04 12:00:00 +0530

    feat(deepskilling/week-3): add Spring Learn and Microservices Account & Loan services

---
+++ deepskilling/week 3/microservices/account/src/main/java/com/cognizant/account/Account.java
@@ -0,0 +1,39 @@
+package com.cognizant.account;
+
+public class Account {
+    private String number;
+    private String type;
+    private int balance;
+
+    public Account() {}
+
+    public Account(String number, String type, int balance) {
+        this.number = number;
+        this.type = type;
+        this.balance = balance;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+}
---
+++ deepskilling/week 3/spring-learn/src/main/java/com/cognizant/springlearn/service/CountryService.java
@@ -0,0 +1,24 @@
+package com.cognizant.springlearn.service;
+
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Service;
+
+import com.cognizant.springlearn.Country;
+
+@Service
+public class CountryService {
+
+    public Country getCountry(String code) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+        @SuppressWarnings("unchecked")
+        List<Country> countries = (List<Country>) context.getBean("countryList");
+        
+        return countries.stream()
+                .filter(country -> country.getCode().equalsIgnoreCase(code))
+                .findFirst()
+                .orElse(null);
+    }
+}
